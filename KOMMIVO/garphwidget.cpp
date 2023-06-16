07905d6324289a9cfc47e3add485822d48a14dee
rrrilya <130274524+rrrilya@users.noreply.github.com>
2023-06-16 14:30:31 +0500

    garphwidget.cpp

--- KOMMIVO/garphwidget.cpp
+++ KOMMIVO/garphwidget.cpp
@@ -13,55 +13,23 @@
 #include <QtMath>
 
 void GraphWidget::reshGraph(const Graph& graph, const PathInfo& optimalPath) {
-
-
-    scene()->clear();
+    clearScene();
 
     int numVertices = graph.getNumVertices();
-
-    const int sceneWidth = 1000;
-    const int sceneHeight = 1000;
-    const QPointF center(sceneWidth / 2, sceneHeight / 2);
-    const qreal radius = qMin(sceneWidth, sceneHeight) * 0.4;
-    const qreal angleIncrement = 2 * M_PI / numVertices;
-
-    std::vector<QPointF> vertexPositions;
-    for (int i = 0; i < numVertices; i++) {
-        qreal angle = i * angleIncrement;
-        qreal x = center.x() + radius * qCos(angle);
-        qreal y = center.y() + radius * qSin(angle);
-        vertexPositions.emplace_back(x, y);
-    }
+    std::vector<QPointF> vertexPositions = calculateVertexPositions(numVertices);
 
     drawEdges(graph, vertexPositions);
-
     drawVertices(vertexPositions);
-
     drawPath(optimalPath.path, vertexPositions);
 }
 
-void GraphWidget::visGraph(const Graph& graph)
-{
-    scene()->clear();
+void GraphWidget::visGraph(const Graph& graph) {
+    clearScene();
 
     int numVertices = graph.getNumVertices();
-
-    const int sceneWidth = 1000;
-    const int sceneHeight = 1000;
-    const QPointF center(sceneWidth / 2, sceneHeight / 2);
-    const qreal radius = qMin(sceneWidth, sceneHeight) * 0.4;
-    const qreal angleIncrement = 2 * M_PI / numVertices;
-
-    std::vector<QPointF> vertexPositions;
-    for (int i = 0; i < numVertices; i++) {
-        qreal angle = i * angleIncrement;
-        qreal x = center.x() + radius * qCos(angle);
-        qreal y = center.y() + radius * qSin(angle);
-        vertexPositions.emplace_back(x, y);
-    }
+    std::vector<QPointF> vertexPositions = calculateVertexPositions(numVertices);
 
     drawEdges(graph, vertexPositions);
-
     drawVertices(vertexPositions);
 }
 
@@ -76,10 +44,10 @@ void GraphWidget::drawEdges(const Graph& graph, const std::vector<QPointF>& vert
             if (weight > 0) {
                 QPointF p1 = vertexPositions[v1];
                 QPointF p2 = vertexPositions[v2];
-                QGraphicsLineItem* line = scene()->addLine(p1.x(), p1.y(), p2.x(), p2.y(), edgePen);
+                QGraphicsLineItem* line = addLine(p1.x(), p1.y(), p2.x(), p2.y(), edgePen);
                 line->setZValue(-1); // Поместить линию под текст
                 QString weightText = QString::number(weight);
-                QGraphicsTextItem* weightLabel = scene()->addText(weightText);
+                QGraphicsTextItem* weightLabel = addText(weightText);
                 weightLabel->setFont(weightFont);
                 weightLabel->setDefaultTextColor(Qt::white);
                 weightLabel->setPos((p1.x() + p2.x()) / 2, (p1.y() + p2.y()) / 2);
@@ -87,7 +55,6 @@ void GraphWidget::drawEdges(const Graph& graph, const std::vector<QPointF>& vert
         }
     }
 }
-
 
 void GraphWidget::drawVertices(const std::vector<QPointF>& vertexPositions) {
     const int numVertices = vertexPositions.size();
@@ -99,9 +66,9 @@ void GraphWidget::drawVertices(const std::vector<QPointF>& vertexPositions) {
         QPointF position = vertexPositions[v];
         QRectF rect(position.x() - vertexRadius, position.y() - vertexRadius,
                     2 * vertexRadius, 2 * vertexRadius);
-        scene()->addEllipse(rect, vertexPen, vertexBrush);
+        addEllipse(rect, vertexPen, vertexBrush);
 
-        QGraphicsTextItem* label = scene()->addText(QString::number(v));
+        QGraphicsTextItem* label = addText(QString::number(v));
         label->setPos(position.x() - vertexLabelOffset, position.y() - vertexLabelOffset);
     }
 }
@@ -120,12 +87,34 @@ void GraphWidget::drawPath(const std::vector<int>& path, const std::vector<QPoin
         int v2 = path[i + 1];
         QPointF p1 = vertexPositions[v1];
         QPointF p2 = vertexPositions[v2];
-        scene()->addLine(p1.x(), p1.y(), p2.x(), p2.y(), pathPen);
+        addLine(p1.x(), p1.y(), p2.x(), p2.y(), pathPen);
     }
 
     int v1 = path.back();
     int v2 = path.front();
     QPointF p1 = vertexPositions[v1];
     QPointF p2 = vertexPositions[v2];
-    scene()->addLine(p1.x(), p1.y(), p2.x(), p2.y(), pathPen);
+    addLine(p1.x(), p1.y(), p2.x(), p2.y(), pathPen);
+}
+
+void GraphWidget::clearScene() {
+    scene()->clear();
+}
+
+std::vector<QPointF> GraphWidget::calculateVertexPositions(int numVertices) {
+    std::vector<QPointF> vertexPositions;
+    const int sceneWidth = 1000;
+    const int sceneHeight = 1000;
+    const QPointF center(sceneWidth / 2, sceneHeight / 2);
+    const qreal radius = qMin(sceneWidth, sceneHeight) * 0.4;
+    const qreal angleIncrement = 2 * M_PI / numVertices;
+
+    for (int i = 0; i < numVertices; i++) {
+        qreal angle = i * angleIncrement;
+        qreal x = center.x() + radius * qCos(angle);
+        qreal y = center.y() + radius * qSin(angle);
+        vertexPositions.emplace_back(x, y);
+    }
+
+    return vertexPositions;
 }
